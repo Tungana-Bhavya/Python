106d50c66b03f3215a27a2a05fef2068e2c80abd
TUNGANA BHAVYA <67670424+Tungana-Bhavya@users.noreply.github.com>
2024-12-30 18:46:59 +0530

    Add files via upload

---
+++ Problems/Leetcode/CREATE_NEW_COLUMN.py
@@ -0,0 +1,8 @@
+Select Data
+-----------------------
+
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary']*2
+    return employees
---
+++ Problems/Leetcode/SELECT_DATA.py
@@ -0,0 +1,8 @@
+Select Data
+-----------------------
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+   df= students.loc[students['student_id']==101,['name','age']]
+   return df
