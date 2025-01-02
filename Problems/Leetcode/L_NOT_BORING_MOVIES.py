919b1f0410d482d64df76f91708575936e7cbcf9
TUNGANA BHAVYA <67670424+Tungana-Bhavya@users.noreply.github.com>
2025-01-02 19:58:17 +0530

    Add files via upload
    Added .py files

---
+++ Problems/Leetcode/L_NOT_BORING_MOVIES.py
@@ -0,0 +1,9 @@
+Not Boring Movies
+-----------------------
+
+import pandas as pd
+
+def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
+    df = cinema[(cinema['id'] %2 != 0) & (cinema['description'] != 'boring')]
+    return df.sort_values(by='rating', ascending =False)
+ 
